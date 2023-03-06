2897cae6f72949a090f2df5a3c91a7e945faf6c5
Zera <marveen@ficha.fr>
2023-03-06 13:04:55 +0530

    edited classes.py

--- utils/classes.py
+++ utils/classes.py
@@ -14,3 +14,20 @@ class_info ={
    'cylinders':12,
    'bag':13
 }
+
+class_id2name ={
+   0:'big cardboard',
+   1:'bulky',
+   2:'compactor',
+   3:'dumpster',
+   4:'electrical and electronic waste',
+   5:'garbage bag',
+   6:'glass',
+   7:'green waste',
+   8:'hygiene',
+   9:'plastic bag',
+   10:'recycle waste',
+   11:'textile',
+   12:'cylinders',
+   13:'bag'
+}
